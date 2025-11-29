94af01267ebbacbddd3c84e58fafba20e80f39f6
Hami0095 <abdman0095@gmail.com>
2025-11-29 23:47:20 +0500

    Reverse Complement and Complement base strand added

--- dna_tool_kit.py
+++ dna_tool_kit.py
@@ -1,5 +1,7 @@
 
 Nucleotides = ['A', 'T', 'C', 'G']
+Complement = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+
 def validateSeq(dna_seq):
     """Validate a DNA sequence.
 
@@ -39,3 +41,28 @@ def transcribeDNAtoRNA(dna_seq):
     if(not validateSeq(dna_seq)):
         raise Exception("Invalid DNA Sequence")
     return dna_seq.upper().replace('T', 'U')    
+
+def reverse_complementDNA(dna_seq):
+    """Get the reverse complement of a DNA sequence.
+
+        Args:
+            dna_seq (str): The DNA sequence.
+    """
+    if(not validateSeq(dna_seq)):
+        raise Exception("Invalid DNA Sequence")
+    tmp = dna_seq.upper()[::-1]
+    rev_comp = ''.join([Complement[nuc] for nuc in tmp])
+    return rev_comp
+
+def complementDna(dna_seq):
+    """Get the complement of a DNA sequence.
+
+        Args:
+            dna_seq (str): The DNA sequence.
+    """
+    if(not validateSeq(dna_seq)):
+        raise Exception("Invalid DNA Sequence")
+    comp = ''.join([Complement[nuc] for nuc in dna_seq.upper()])
+    return comp
+
+
